6a403bfa5d561a85f9b7577c635e377b4627ce27
jinio <jinioh88@gmail.com>
2020-02-18 08:33:04 +0900

    add: 프론트 백앤드 연결

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/api/*': {
+        target: 'http//localhost:8080'
+      }
+    }
+  }
 }
--- src/main/java/com/taskagile/app/web/MainController.java
+++ src/main/java/com/taskagile/app/web/MainController.java
@@ -1,4 +1,12 @@
 package com.taskagile.app.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+  @GetMapping(value = {"/", "/login"})
+  public String entry() {
+    return "index";
+  }
 }
